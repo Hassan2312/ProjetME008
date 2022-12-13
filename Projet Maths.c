b6426245cb4afadeadf570956ab9317d8e816a9b
Hassan2312 <78531010+Hassan2312@users.noreply.github.com>
2022-12-13 16:49:00 +0100

    Update Projet Maths.c

--- Projet Maths.c
+++ Projet Maths.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #define n 101
+#define taille 200000
 
-void afficher_vect(float x[n]) {
+void afficher_vect(float x[n], int nmax) {
   int i;
   printf("--------------------------------------\n");
-  for (i = 0; i < n; i++)
+  for (i = 0; i < nmax; i++)
     printf("%f\n", x[i]);
   printf("--------------------------------------\n");
 }
@@ -34,7 +35,10 @@ void initU(float Un[n]) {
   Un[49] = 5.5;
 }
 
-void updateUn(float a[n], float c[n], float d[n], float Un[n]) {
+void updateUn(float Un[n], float dt, float dx) {
+  float a[n], c[n], d[n];
+  float h = dt / (dx * dx);
+  init_A(h, a, c, d);
   float Un1[n];
   Un1[0] = d[0] * Un[0] + c[0] * Un[1];
   Un1[n - 1] = d[n - 1] * Un[n - 1] + a[n - 1] * Un[n - 2];
@@ -45,15 +49,21 @@ void updateUn(float a[n], float c[n], float d[n], float Un[n]) {
 }
 
 void Un_instant_t (float Unt[n], float instant, float dt, float dx) {
-  float a[n], c[n], d[n];
-  float l[n], u[n], v[n];
-  float h = dt / (dx * dx);
-  init_A(h, a, c, d);
   initU(Unt);
   for (int i = 0; i * dt < instant; i++) {
-    updateUn(a, c, d, Unt);
+    updateUn(Unt, dt, dx);
     }
 }
+
+void Un_x_fixe (float Ut_x[taille], int position, float dt, float dx) {
+  float Unt[n];
+  initU(Unt);
+  for(int i=0; i<taille; i++) {
+    Ut_x[i]=Unt[position];
+    updateUn(Unt, dt, dx);
+  }
+}
+
 
 int main(void) {
 
@@ -61,7 +71,7 @@ int main(void) {
   scanf("%f", &dt);
   scanf("%f", &dx);
 
-  float  Un1[n], Un2[n], Un3[n], Un4[n], Un20[n];
+  float  Un1[n], Un2[n], Un3[n], Un4[n], Un20[n], Ut_10[taille];
 
   
 
@@ -76,9 +86,10 @@ int main(void) {
     fprintf(out1,"%f\t%f\n", i*dx-10, Un1[i]);
   }
   fclose(out1);
-  
-  afficher_vect(Un1);
-  afficher_vect(Un20);
+  Un_x_fixe (Ut_10, 10, dt, dx);
+  afficher_vect(Ut_10, 20);
+  //afficher_vect(Un20, n);
   
   return 0;
 }
+ 
